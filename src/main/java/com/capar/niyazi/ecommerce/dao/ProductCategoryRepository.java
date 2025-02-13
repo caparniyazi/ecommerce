d2592cb197d40ae44fed07d8a3d2605bb8a0d412
caparniyazi <capar.niyazi.tr@gmail.com>
2025-02-13 11:08:35 +0300

    CORS.

--- src/main/java/com/capar/niyazi/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/capar/niyazi/ecommerce/dao/ProductCategoryRepository.java
@@ -3,11 +3,13 @@ package com.capar.niyazi.ecommerce.dao;
 import com.capar.niyazi.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * collectionResourceRel: Name of JSON entry
  * product-category: Name of the reference path.
  */
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
