9913696783e4d76389f2155de106b6af2036d980
caparniyazi <capar.niyazi.tr@gmail.com>
2025-02-20 19:15:47 +0300

    New query methods.

--- src/main/java/com/capar/niyazi/ecommerce/config/DataRestConfig.java
+++ src/main/java/com/capar/niyazi/ecommerce/config/DataRestConfig.java
@@ -2,14 +2,29 @@ package com.capar.niyazi.ecommerce.config;
 
 import com.capar.niyazi.ecommerce.entity.Product;
 import com.capar.niyazi.ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
+    // Data fields
+    private final EntityManager em;
+
+    @Autowired
+    public DataRestConfig(EntityManager em) {
+        this.em = em;
+    }
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] theUnsupportedMethods = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
@@ -25,5 +40,24 @@ public class DataRestConfig implements RepositoryRestConfigurer {
                         ((metdata, httpMethods) -> httpMethods.disable(theUnsupportedMethods))
                 )
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedMethods));
+
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        // Get a list of all entity classes from the entity manager.
+        Set<EntityType<?>> entityTypes = em.getMetamodel().getEntities();
+
+        // Create an array of the entity types.
+        List<Class<?>> entityClasses = new ArrayList<>();
+
+        // Get the entity types for the entities.
+        for (EntityType<?> entityType : entityTypes) {
+            entityClasses.add(entityType.getJavaType());
+        }
+
+        // Expose the entity ids for the array of entity/domain types.
+        Class<?>[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
--- src/main/java/com/capar/niyazi/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/capar/niyazi/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,20 @@
 package com.capar.niyazi.ecommerce.dao;
 
 import com.capar.niyazi.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    // A query method.
+    // SELECT * FROM product where category_id=?
+    // Spring Data REST automatically exposes end-point
+    // http://localhost:8080/api/v1/products/search/findByCategoryId?id=2, for example.
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+    // SELECT * FROM Product p WHERE p.name like CONCAT('%', :name, '%')
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
 }
